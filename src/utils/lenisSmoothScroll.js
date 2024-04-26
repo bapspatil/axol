c60010f9ccef127f9dec0c1ec1838cdab2de0942
bapspatil <bapoo1998@gmail.com>
2024-04-26 10:04:53 +0530

    Added smooth scrolling for internal links

--- src/utils/lenisSmoothScroll.js
+++ src/utils/lenisSmoothScroll.js
@@ -1,4 +1,16 @@
 const initSmoothScrolling = () => {
+    // fix for id links
+    document.querySelectorAll('a[href^="#"]').forEach((el) => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault()
+            const id = el.getAttribute('href')?.slice(1)
+            if (!id) return
+            const target = document.getElementById(id)
+            if (target) {
+                target.scrollIntoView({ behavior: 'smooth' })
+            }
+        })
+    })
     const lenis = new Lenis({
         lerp: 0.1,
         duration: 1.5,
