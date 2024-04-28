ed81c284dc734e53f00ced161de4c8c608a117f4
Bapusaheb Patil <bapoo1998@gmail.com>
2024-04-28 19:55:30 +0530

    Added Cloudflare to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx(), sitemap(), partytown()]
+  integrations: [react(), tailwind(), mdx(), sitemap(), partytown()],
+  output: "server",
+  adapter: cloudflare(),
 });
