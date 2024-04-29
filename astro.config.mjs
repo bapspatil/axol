ec462c66439a08233653ff5cb95076e9385ba61f
Bapusaheb Patil <bapoo1998@gmail.com>
2024-04-30 03:05:58 +0530

    Added Contact section

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
@@ -12,4 +11,9 @@ export default defineConfig({
   integrations: [react(), tailwind(), mdx(), sitemap(), partytown()],
   output: "server",
   adapter: cloudflare(),
+  // vite: {
+  //   define: {
+  //     'process.env.CLOUDFLARE_TURNSTILE_SITEKEY': JSON.stringify(process.env.CLOUDFLARE_TURNSTILE_SITEKEY)
+  //   }
+  // }
 });
